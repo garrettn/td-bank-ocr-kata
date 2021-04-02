d5b9d470e094d68bb1cee1de6eede18a40beb5c8
Garrett Nay <g@garrettnay.com>
2021-04-01 21:29:27 -0600

    Set up the basic idea of how to structure the functions

--- src/decode-numbers.js
+++ src/decode-numbers.js
@@ -1,6 +1,24 @@
+// Top-level function
 function decodeNumbers(rawData) {
-  return rawData
+  const entries = getEntries(rawData)
+  const decodedEntries = entries.map(decodeEntry)
+  return decodedEntries
 }
+
+// Parse data into an array of arrays, where each
+// element of the inner array is the "flattened" digit
+function getEntries(data) {
+  return []
+}
+
+// Decode the entire entry of flattened digits
+function decodeEntry(entry) {
+  const decodedDigits = entry.map(decodeDigit)
+  return decodedDigits.join('')
+}
+
+// Decode an individual digit
+function decodeDigit(digit) {}
 
 module.exports = {
   decodeNumbers,
